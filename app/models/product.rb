15313d3bbf7767ce856e9a7219104f911e4d21f9
senthgit7th <ryo0514s.0707@outlook.jp>
2020-02-03 19:13:28 +0900

    productモデルのアソシエーションの記述

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :images, dependent: :destroy
+  belongs_to :user
+  belongs_to :category
+  has_one :delivery
+  has_one :todo
 end
