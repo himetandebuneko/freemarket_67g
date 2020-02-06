f5b17b42548a600b26daca588d57c63125f6da41
himetandebuneko <ryunosukeman@gmail.com>
2020-02-06 18:36:05 +0900

    商品出品ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'products#index'
-  resources :products, only: [:show]
+  resources :products, only: [:show, :new]
   resources :users, only: [:index, :create, :show]  
 end
