e3ca8ab82953a5e9a81f0f4f6a32227feffc1c34
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-02-06 10:54:30 +0900

    third commit
    ログアウトページ作成（ルート作成、HTML、SCSS）
    application.scss内の記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'products#index'
   resources :products, only: [:show]
-  resources :users, only: [:show]  
+  resources :users, only: [:index, :show]  
 end
