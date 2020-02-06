8d86b1cb3024fecf724139716b094d75b98ff6bb
gangelion <gangelion8@gmail.com>
2020-02-06 20:13:37 +0900

    imagesコントローラーの作成

--- db/migrate/20200206100742_create_images.rb
+++ db/migrate/20200206100742_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.string :image, null: false
-      # t.reference :product, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2020_02_06_100742) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_images_on_product_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_02_06_100742) do
     t.index ["name"], name: "index_products_on_name"
   end
 
+  add_foreign_key "images", "products"
 end
