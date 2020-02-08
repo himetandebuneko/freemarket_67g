b6af742b0ca0bb571d3860ed22a046525f5e7dbf
yoshinori-nishinaka <kakikukeko0526@gmail.com>
2020-02-08 20:22:16 +0900

    カード登録ページのマークアップ
    カード登録ページのマークアップ

--- app/controllers/credits_controller.rb
+++ app/controllers/credits_controller.rb
@@ -1,11 +1,18 @@
 class CreditsController < ApplicationController
   
+  require 'payjp'
+
   def new
     
   end
 
   def create
-    
+    Payjp.api_key = ""
+    Payjp::Charge.create(
+      amount: 1100, # 決済する値段
+      card: params['payjp-token'], # フォームを送信すると生成されるトークン
+      currency: 'jpy'
+    )
   end
 
   def show
--- app/helpers/credits_helper.rb
+++ app/helpers/credits_helper.rb
@@ -1,2 +1,2 @@
-module CredtsHelper
+module CreditsHelper
 end
