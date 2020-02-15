46b9c6f8f0c60d29593e773ea45f103f7a527626
gangelion <gangelion8@gmail.com>
2020-02-15 11:57:31 +0900

    seedファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -79,12 +79,13 @@ ActiveRecord::Schema.define(version: 2020_02_14_102438) do
     t.string "detail", null: false
     t.string "size", null: false
     t.integer "price", null: false
+    t.string "status"
     t.string "condition", null: false
     t.string "shippingaddress", null: false
     t.string "shippingdate", null: false
-    t.string "buyer", null: false
-    t.string "seller", null: false
-    t.string "status"
+    t.string "buyer"
+    t.string "seller"
+    t.string "payer"
     t.bigint "user_id", null: false
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
@@ -110,6 +111,8 @@ ActiveRecord::Schema.define(version: 2020_02_14_102438) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "products"
