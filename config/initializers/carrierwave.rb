e9213b14a10378c8e0c362dfb8524e2274585019
himetandebuneko <ryunosukeman@gmail.com>
2020-02-17 20:29:26 +0900

    バケット名

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,6 +15,6 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'freemarket67g'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/tech54-mercari-a'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket67g'
   end
 end
