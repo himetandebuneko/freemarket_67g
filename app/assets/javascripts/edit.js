c1cf5b989bc41a665d42be16938d9111be284230
gangelion <gangelion8@gmail.com>
2020-02-19 12:09:57 +0900

    商品編集画面で画像を変更できるように実装

--- app/assets/javascripts/edit.js
+++ app/assets/javascripts/edit.js
@@ -1,14 +1,11 @@
 $(document).on('turbolinks:load', ()=> {
   // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
-    const html = `<div class="js-file_group" data-id="${num}">
-                    <input class="js-file" type="file" name="product[images_attributes][${num}][image]" id="product_images_attributes_${num}_image">
-                    <span class="js-remove">削除</span>
-                  </div>`;
+    const html = `<input data-id="${num}" class="file_field" type="file" name="product[images_attributes][${num}][image]" id="product_images_attributes_${num}_image">`;
     return html;
   }
   const buildImg = (index, url)=> {
-    const html = `<img class="preview" data-id="${index}" src="${url}" width="100px" height="100px">`;
+    const html = `<img data-id="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
   // file_fieldのnameに動的なindexをつける為の配列
@@ -18,32 +15,32 @@ $(document).on('turbolinks:load', ()=> {
 
   $('.hidden-destroy').hide();
 
-  $('#image-box').on('change', '.js-file', function(e) {
-    const targetIndex = $(this).parent().data('id');
+  $('input').on('change', 'js-remove', function(e) {
+    const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
 
     if (img = $(`img[data-id="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
     } else {
-      $('.sell-upload-items-container').append(buildImg(targetIndex, blobUrl));
+      $('img:last').after(buildImg(targetIndex, blobUrl));
       // fileIndexの先頭の数字を使ってinputを作る
-      $('#previews').append(buildFileField(fileIndex[0]));
+      $('.edit-sub_image__ultext').append(buildFileField(fileIndex[0]));
       fileIndex.shift();
       // 末尾の数に1足した数を追加する
-      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)    
-
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)     
     }
   });
 
-  $('#image-box').on('click', '.js-remove', function() {
+  $('.js-remove').on('click', function() {
     const targetIndex = $(this).parent().data('id');
     const hiddenCheck = $(`input[data-id="${targetIndex}"].hidden-destroy`);
+    debugger
     if (hiddenCheck) hiddenCheck.prop('checked', true);
-    $(this).parent().prev().remove();
-    $(this).parent().remove();
-    $(`img[data-id="${targetIndex}"]`).remove();
+    $(this).prev().remove();
+    $(this).remove();
+    // $(`img[data-id="${targetIndex}"]`).remove();
     // 画像入力欄が0個にならないようにしておく
-    if ($('.js-file').length == 0) $('#preview').append(buildFileField(fileIndex[0]));
+    if ($('input').length == 0) $('.edit-first_content').append(buildFileField(fileIndex[0]));
   });
 });
